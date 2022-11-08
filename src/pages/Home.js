0a0d36bd8046bc826a0146811b843deed121dd8d
Seu nome <seuemail@exemplo.br>
2022-11-08 17:15:13 -0300

    feat(pages/Home): import Banner in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../components/components-home/Banner';
 import Header from '../components/Header';
 
 function Home() {
   return(
     <div>
       <Header />
+      <Banner />
     </div>
   );
 }
