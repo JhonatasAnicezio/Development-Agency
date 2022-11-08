5683ddb0803f523f707c452c6d315f53bcc5c31c
Seu nome <seuemail@exemplo.br>
2022-11-08 13:44:40 -0300

    feat(services): create services

---
+++ src/services.js
@@ -0,0 +1,27 @@
+import banner1 from './img/banner1.png';
+import banner2 from './img/banner2.png';
+import banner3 from './img/banner3.png';
+
+export default listBanner = [
+  {
+    title: 'Quer criar um e-commerce?',
+    description: 'Conte conosco, temos mais de 30 lojas no portifólio.',
+    img: banner1,
+    alt: 'Projetos de e-commerce',
+    link: 'Ver portifólio',
+  },
+  {
+    title: 'Esta querendo tirar o projeto do papel?',
+    description: 'Nossa equipe de engenharia de software está pronta para lhe atender.',
+    img: banner2,
+    alt: 'Engenharia de Software',
+    link: 'Entre em contato',
+  },
+  {
+    title: 'Está buscando manuntenção no seu Software?',
+    description: 'A hDC conta com engenheiros altamente qualificados para resolver seu problema.',
+    img: banner3,
+    alt: 'Manuntenção em Software',
+    link: 'Etre em contato',
+  },
+]
