71208bb54e2e8019182301bbadfb834b9de7fed1
Seu nome <seuemail@exemplo.br>
2022-11-08 13:10:38 -0300

    feat(App): create routes for App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Header from './components/Header';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={ Home } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
