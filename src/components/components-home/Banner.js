7eccc95a2f00f2163b695a1aad1c047bc621c7eb
Seu nome <seuemail@exemplo.br>
2022-11-08 17:12:58 -0300

    feat(components-home/Banner): create component Banner

---
+++ src/components/components-home/Banner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import { listBanner } from '../../services';
+
+function Banner() {
+  return (
+    <Carousel>
+      { listBanner.map((banner, index) =>
+        <Carousel.Item key={ index }>
+          <img
+            className='d-block w-100'
+            src={ banner.img }
+            alt={ banner.alt }
+          />
+          <Carousel.Caption>
+            <h2>{ banner.title }</h2>
+            <p>{ banner.description }</p>
+            <a href='#' className='main-btn'>{ banner.link }</a>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ) }
+    </Carousel>
+  );
+}
+
+export default Banner;
