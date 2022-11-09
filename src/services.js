f7ac60d63ff32aa81f22dc64eabc04e583916d31
Seu nome <seuemail@exemplo.br>
2022-11-09 15:44:34 -0300

    feat(services): create arr for especialidades

--- src/services.js
+++ src/services.js
@@ -2,6 +2,11 @@ import banner1 from './img/banner1.png';
 import banner2 from './img/banner2.png';
 import banner3 from './img/banner3.png';
 
+import {
+  faMobileAlt, faShoppingCart, faPaintBrush,
+  faGlobe, faHandsHelping, faServer,
+} from '@fortawesome/free-solid-svg-icons';
+
 export const listBanner = [
   {
     title: 'Quer criar um e-commerce?',
@@ -65,4 +70,37 @@ export const diferenciais = [
   'Integração com diversos sistemas no mercado',
   'Sistema de pagamento próprio',
   'Desenvolvimento com metodologia ágil',
+];
+
+export const especialidades = [
+  {
+    icon: faMobileAlt,
+    title: 'Aplicativos Nativos',
+    description: 'Utilizamos o real poder dos dispositivos móveis construindo aplicativos nativos.',
+  },
+  {
+    icon: faShoppingCart,
+    title: 'E-commerces',
+    description: 'Temos uma plataforma própria de e-commerce que se adapta a qualquer regra de negócio(B2C e B2B).',
+  },
+  {
+    icon: faPaintBrush,
+    title: 'Design Criativo',
+    description: 'Nossos designs são especializados em UI/UX e tambem possuem vasta experiência no mercado de software.',
+  },
+  {
+    icon: faGlobe,
+    title: 'Experts em SEO',
+    description: 'Com nosso serviço de marketing seu site será melhor posicionado no Google e venderá mais.',
+  },
+  {
+    icon: faHandsHelping,
+    title: 'Suporte 24/7',
+    description: 'Você sempre tera alguem da equipe disponivel para suporte, em diferentes plataformas. (chat, email, skype).',
+  },
+  {
+    icon: faServer,
+    title: 'Experiência em servidores',
+    description: 'Garantimos uptime e também rápido tempo de resposta do seu site para o seu cliente.',
+  },
 ];
