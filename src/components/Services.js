abbd37c4df3865f59acf5fe5234bb497ab346e66
Seu nome <seuemail@exemplo.br>
2022-11-09 15:47:48 -0300

    feat(components/Services): add itens in components

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { especialidades } from '../services';
+import TitleH2 from './TitleH2';
 
 function Services() {
   return (
-    <div>
-
+    <div id='services-area'>
+      <div className='container'>
+        <div className='row'>
+          <TitleH2 title='Nossas especialidades' />
+        </div>
+        <div className='d-flex flex-wrap'>
+          { especialidades.map((especialidade, index) =>
+            <div className='col-md-4 service-box' key={ index }>
+              <FontAwesomeIcon icon={ especialidade.icon } />
+              <h4>{ especialidade.title }</h4>
+              <p>{ especialidade.description }</p>
+            </div>
+          ) }
+        </div>
+      </div>
     </div>
   );
 }
