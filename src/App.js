f584dfd1bf04c45e88d4606481df49254dffd6a2
Seu nome <seuemail@exemplo.br>
2022-11-09 15:45:11 -0300

    feat(App): add div container fluid

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import About from './components/About';
 import Banner from './components/Banner';
 import Header from './components/Header';
+import Services from './components/Services';
 
 function App() {
   return (
     <div>
       <Header />
       <Banner />
-      <About />
+      <div className='container-fluid'>
+        <About />
+        <Services />
+      </div>
     </div>
   );
 }
