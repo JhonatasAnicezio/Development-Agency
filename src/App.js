7db2c9b9924845ea685171f824363ba7cba25d51
Seu nome <seuemail@exemplo.br>
2022-11-13 13:20:45 -0300

    feat(App): import Portifolio

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Services from './components/Services';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import Time from './components/Time';
 import Company from './components/Company';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Time />
         <Company />
       </ParallaxProvider>
+      <Portfolio />
     </div>
   );
 }
